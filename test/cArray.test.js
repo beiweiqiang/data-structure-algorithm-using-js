9feda88d48d51f5c60268de22202afe50e0447b7
beiweiqiang <tribal.bei@onedollartech.com>
2018-05-17 07:53:41 +0800

    添加希尔排序

--- test/cArray.test.js
+++ test/cArray.test.js
@@ -6,29 +6,62 @@ it.skip('CArray', () => {
   console.log(nums.toString());
 });
 
+it.skip('bubble sort order', () => {
+  const nums = new CArray(10);
+  nums.setData();
+  nums.bubbleSort();
+  console.log('bubble sort: ', nums.dataStore);
+});
 it('bubble sort', () => {
   const nums = new CArray(20000);
   nums.setData();
   const start = new Date().getTime();
   nums.bubbleSort();
   const end = new Date().getTime();
-  console.log(`bubble: ${end - start}`);
+  console.log(`bubble speed: ${end - start}`);
 });
 
+it('selection sort order', () => {
+  const nums = new CArray(10);
+  nums.setData();
+  nums.selectionSort();
+  console.log('selection sort: ', nums.dataStore);
+});
 it('selection sort', () => {
   const nums = new CArray(20000);
   nums.setData();
   const start = new Date().getTime();
   nums.selectionSort();
   const end = new Date().getTime();
-  console.log(`selection: ${end - start}`);
+  console.log(`selection speed: ${end - start}`);
 });
 
-it('insertion sort', () => {
+it('insertion sort order', () => {
+  const nums = new CArray(10);
+  nums.setData();
+  nums.insertionSort();
+  console.log('insertion sort: ', nums.dataStore);
+});
+it('insertion sort test speed', () => {
   const nums = new CArray(20000);
   nums.setData();
   const start = new Date().getTime();
   nums.insertionSort();
   const end = new Date().getTime();
-  console.log(`insertion: ${end - start}`);
+  console.log(`insertion speed: ${end - start}`);
+});
+
+it('shell sort order', () => {
+  const nums = new CArray(10);
+  nums.setData();
+  nums.shellSort();
+  console.log('shell sort: ', nums.dataStore);
+});
+it('shell sort test speed', () => {
+  const nums = new CArray(200000);
+  nums.setData();
+  const start = new Date().getTime();
+  nums.shellSort();
+  const end = new Date().getTime();
+  console.log(`shell speed: ${end - start}`);
 });
